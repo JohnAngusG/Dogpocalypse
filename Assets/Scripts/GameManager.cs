406a7b08e5a9fc5b2338ba07cfd93401293348a2
John Mullaney <j.a.mullaney1@gmail.com>
2025-01-30 19:21:23 -0800

    Debugging Animation
    Not fixed yet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,19 +5,29 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject dog;
     private DogMovement dm;
+    private Animator animator;
 
     private void Start()
     {
         dm = dog.GetComponent<DogMovement>();
+        animator = dog.GetComponent<Animator>();
     }
 
     public void Freeze() {
         dm.enabled = false;
+        animator.SetBool("Caught", true);
+        animator.SetFloat("Speed", 0);
         StartCoroutine(UnFreeze());
     }
 
     private IEnumerator UnFreeze() {
         yield return new WaitForSeconds(2);
         dm.enabled = true;
+        animator.SetBool("Caught", false);
     }
+
+
+
+
+
 }
