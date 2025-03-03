307fc14e58671573bfc9e39acd97486332a5b4d3
John Mullaney <j.a.mullaney1@gmail.com>
2025-03-03 08:53:12 -0800

    Add audio files and level design

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,23 +4,24 @@ public class PickUp : MonoBehaviour
 {
     [SerializeField] private GameManager gm;
     [SerializeField] private float degrees = 90f;
+    [SerializeField] GameObject model;
 
     private void OnTriggerEnter(Collider other)
     {
         gm.Pickup();
-        // gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 
 
     private void Start()
     {
-        transform.Rotate(new Vector3(45, 0, 0));
+        model.transform.Rotate(new Vector3(45, 0, 0));
     }
 
     private void Update()
     {
         Vector3 rot = Vector3.up * degrees * Time.deltaTime;
-        transform.Rotate(rot);  
+        model. transform.Rotate(rot);  
     }
 
 
